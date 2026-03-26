58546393f1a32d82cdd634b3e169c28453421647
Henrique Takagi <henrique.takagi@aluno.senai.br>
2026-03-25 21:44:15 -0400

    Quinta commit

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,8 +1,33 @@
+import { supabase } from '@/lib/supabase';
+import { useEffect, useState } from 'react';
+import 'react-native-url-polyfill/auto';
+
 import Login from '@/components/frm-login';
 
-export default function Auth(){
-    return(
-        <Login></Login>
+import { JwtPayload } from '@supabase/supabase-js';
+import { Text, View } from 'react-native';
+
+export default function Auth() {
+    const [claims, setClaims] = useState<JwtPayload | null>(null)
+    useEffect(() => {
+        supabase.auth.getClaims().then(({ data }) => {
+            if (data) {
+                setClaims(claims)
+            }
+        })
+        supabase.auth.onAuthStateChange(() => {
+            supabase.auth.getClaims().then(({ data }) => {
+                if (data) {
+                    setClaims(claims)
+                }
+            })
+        })
+    }, [])
+    return (
+        <View>
+            <Login></Login>
+            {claims && <Text>{claims.sub}</Text>}
+        </View>
     );
 }
 
--- components/frm-login.tsx
+++ components/frm-login.tsx
@@ -1,15 +1,19 @@
-import { 
-    View, 
-    Text, 
-    TextInput, 
-    TouchableOpacity, 
-    StyleSheet, 
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
 } from 'react-native';
 
 import { useState } from 'react';
 //npm install react-native-toast-message
+import { supabase } from '@/lib/supabase';
 import Toast from 'react-native-toast-message';
-export default function Login(){
+
+import { router } from 'expo-router';
+export default function Login() {
+    /*
     const [usuario, setUsuario] = useState('');
     const [senha, setSenha] = useState('');
 
@@ -28,20 +32,67 @@ export default function Login(){
             })
             //subir para o git
         }
+    }*/
+    const [usuario, setUsuario] = useState('')
+    const [senha, setSenha] = useState('')
+    const [loading, setLoading] = useState(false)
+    async function validarLogin() {
+        setLoading(true)
+        const { error } = await supabase.auth.signInWithPassword({
+            email: usuario,
+            password: senha,
+        })
+        // if (error) Alert.alert(error.message)
+        if (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: error.message
+            })
+        }
+        setLoading(false)
+        router.replace('/tabs');
+    }
+    async function signUpWithEmail() {
+        setLoading(true)
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: usuario,
+            password: senha,
+        })
+        // if (error) Alert.alert(error.message)
+        if (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: error.message
+            })
+        }
+        // if (!session) Alert.alert('Please check your inbox for email verification!')
+        if (!session) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: 'Verifique na sua caixa de e-mail a verificação'
+            })
+        }
+        setLoading(false)
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <Text style={styles.Text}> Área Restrita</Text>
-            
-            <TextInput 
-                style={styles.Input} 
+
+            <TextInput
+                style={styles.Input}
                 placeholder="Informe seu usuário"
                 value={usuario}
                 onChangeText={setUsuario}
             />
 
-            <TextInput 
+            <TextInput
                 style={styles.Input}
                 placeholder="Informe sua senha"
                 value={senha}
@@ -51,7 +102,7 @@ export default function Login(){
 
             <Toast />
 
-            <TouchableOpacity style={styles.Button} onPress={validarLogin}>
+            <TouchableOpacity style={styles.Button} onPress={validarLogin} disabled={loading}>
                 <Text style={styles.Text}>Entrar</Text>
             </TouchableOpacity>
         </View>
@@ -60,24 +111,24 @@ export default function Login(){
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         backgroundColor: 'green',
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
     },
-    Text:{
+    Text: {
         fontSize: 24, color: '#ffffff',
-        marginBottom:20,
+        marginBottom: 20,
     },
-    Input:{
+    Input: {
         width: '100%',
         height: 40,
         backgroundColor: '#ffffff',
-        marginBottom:20,
+        marginBottom: 20,
         color: '#000000'
     },
-    Button:{
+    Button: {
         width: '100%',
         height: 40,
         backgroundColor: '#c2e015',
